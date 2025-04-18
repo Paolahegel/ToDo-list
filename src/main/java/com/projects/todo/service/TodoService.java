f520da0b25b4b58d4c65e72408316e88de490780
paolamoura <paolamoura.kj@hotmail.com>
2025-04-18 20:03:12 -0300

    feat: defined CRUD in controller

---
+++ src/main/java/com/projects/todo/service/TodoService.java
@@ -0,0 +1,39 @@
+package com.projects.todo.service;
+
+import com.projects.todo.entity.Todo;
+import com.projects.todo.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+        Sort sort = Sort.by("priority").descending()
+                .and(Sort.by("nome").ascending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+
+}
