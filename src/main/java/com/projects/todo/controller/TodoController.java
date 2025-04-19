2ecf280003b6f857d22e21116c3714d6365905b7
paolamoura <paolamoura.kj@hotmail.com>
2025-04-18 23:39:35 -0300

    test: add unit tests for TodoController(create)

--- src/main/java/com/projects/todo/controller/TodoController.java
+++ src/main/java/com/projects/todo/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.projects.todo.controller;
 
 import com.projects.todo.entity.Todo;
 import com.projects.todo.service.TodoService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,7 +17,7 @@ public class TodoController {
     }
 
     @PostMapping
-    List<Todo> create(@RequestBody Todo todo){
+    List<Todo> create(@RequestBody @Valid Todo todo){
         return todoService.create(todo);
     }
 
--- src/main/java/com/projects/todo/entity/Todo.java
+++ src/main/java/com/projects/todo/entity/Todo.java
@@ -1,6 +1,8 @@
 package com.projects.todo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,11 +14,21 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     private String nome;
+
+    @NotBlank
     private String descricao;
 
     @Column(name = "realizado", nullable = false)
     private boolean realizado;
     private int prioridade;
 
+    public Todo(String nome, String descricao, boolean realizado, int prioridade) {
+        this.nome = nome;
+        this.realizado = realizado;
+        this.descricao = descricao;
+        this.prioridade = prioridade;
+    }
 }
