1eaf58812d1cdac2b54e85c88041283b8c5a37b6
paolamoura <paolamoura.kj@hotmail.com>
2025-04-18 21:03:47 -0300

    refactor: remove unused classes

--- src/main/java/com/projects/todo/repository/TaskListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projects.todo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface TaskListRepository extends JpaRepository<TaskList, Long> {
-    List<TaskList> findByProject(Project project);
-}
