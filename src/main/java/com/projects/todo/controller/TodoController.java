7f98e3f618995414f1c067d3ac273b30d0043bb4
paolamoura <paolamoura.kj@hotmail.com>
2025-04-19 22:44:36 -0300

    docs: add Swagger descriptions for API requests

--- src/main/java/com/projects/todo/controller/TodoController.java
+++ src/main/java/com/projects/todo/controller/TodoController.java
@@ -3,13 +3,14 @@ package com.projects.todo.controller;
 import com.projects.todo.entity.Todo;
 import com.projects.todo.entity.enums.TodoStatus;
 import com.projects.todo.service.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/todos")
+@RequestMapping("/api/todo")
 public class TodoController {
     private TodoService todoService;
 
@@ -17,26 +18,31 @@ public class TodoController {
         this.todoService = todoService;
     }
 
+    @Operation(summary = "create a new task")
     @PostMapping
     List<Todo> create(@RequestBody @Valid Todo todo){
         return todoService.create(todo);
     }
 
+    @Operation(summary = "returns all created tasks")
     @GetMapping
     List<Todo> list(){
         return todoService.list();
     }
 
+    @Operation(summary = "update a task")
     @PutMapping
     List<Todo> update(@RequestBody Todo todo){
         return todoService.update(todo);
     }
 
+    @Operation(summary = "delete a task by id")
     @DeleteMapping("{id}")
     List<Todo> delete(@PathVariable("id") Long id){
         return todoService.delete(id);
     }
 
+    @Operation(summary = "filter tasks by status")
     @GetMapping("{status}")
     List<Todo> filterByStatus(@PathVariable("status") TodoStatus todoStatus){
         return todoService.findByStatus(todoStatus);
